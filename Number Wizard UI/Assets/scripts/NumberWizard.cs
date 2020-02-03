e7afb4424287e332182bec7074103e1a7d5b0997
marco <marco.py@yahoo.com>
2020-02-02 20:06:26 -0500

    first commit

---
+++ Number Wizard UI/Assets/scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+	[SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+	int guess;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartGame();
+	}
+
+	void StartGame()
+	{
+
+        NextGuess();
+        
+
+    }
+
+	// Update is called once per frame
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+	void NextGuess()
+	{
+        guess = Random.Range(min,max);
+        guessText.text = guess.ToString();
+    }
+}
+
---
+++ Trapped Underground/Assets/scripts/sceneloader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneloader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void SceneA1()
+    {
+        SceneManager.LoadScene("Situation A1");
+    }
+    public void SceneB1()
+    {
+        SceneManager.LoadScene("Situation B1");
+    }
+    public void endgame()
+    {
+        SceneManager.LoadScene("end");
+    }
+    public void SceneB2()
+    {
+        SceneManager.LoadScene("situation B2");
+    }
+    public void SceneVictory()
+    {
+        SceneManager.LoadScene("Victory Scene");
+    }
+
+}
